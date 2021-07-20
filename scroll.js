71e5be9854abd1b81bf8943877e3eed550f6729c
diegoztrombone <diegoztrombone@gmail.com>
2021-07-20 16:19:32 +0200

    feat: ejercicio para Bewor-Tech

---
+++ scroll.js
@@ -0,0 +1,34 @@
+const listNode = document.querySelector('#listWrapper')
+const btnNode = document.querySelector('#myBtn')
+const apiURL = 'https://jsonplaceholder.typicode.com/posts'
+
+
+const fetchFn = url => fetch(url).then(res => res.json())
+
+const counterObj = {
+    counter: 0
+}
+const dataCB = data => {
+    if (data.id > counterObj.counter) {
+        return
+    }
+    listNode.innerHTML += printFn(data)
+}
+
+const printFn = data => {
+    return `<div class="tarjetClass">
+                <p>${data.title}</p>
+            </div>`
+}
+
+
+btnNode.addEventListener('click', () => {
+    counterObj.counter += 10
+    fetchFn(apiURL).then(data => {
+        data.map(dataCB)
+        return data
+    })
+
+    listNode.scrollTop = listNode.scrollHeight
+})
+
